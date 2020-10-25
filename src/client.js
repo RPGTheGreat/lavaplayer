f808cd3ff8270b5207ced5b48b9dc39498411645
RPGTheGreat <54214792+RPGTheGreat@users.noreply.github.com>
2020-10-25 14:28:50 +0530

    Create client.js

--- src/client.js
+++ src/client.js
@@ -8,7 +8,8 @@ class Client {
 	 * @param {Object} options
 	 */
 	constructor(key) {
-	
+if(!key) throw new Error("[YT_API_KEY] Invalid YT_API_KEY"
+if (typeof key !== "string") throw new Error("[YT_API_KEY] YT_API_KEY Must be a string")
     this.key = key
     
 		this.queue = new Map();
